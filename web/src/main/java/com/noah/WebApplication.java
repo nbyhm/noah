804435c64a159d85cc1edb4c984c0e2a0fe689f0
nbyhm <39179650+nbyhm@users.noreply.github.com>
2018-11-04 19:32:16 +0800

    SpringBoot打包方式为war包时， 1. 		<!-- 增加依赖覆盖默认内嵌的Tomcat依赖 --> 		<dependency> 			<groupId>org.springframework.boot</groupId> 			<artifactId>spring-boot-starter-tomcat</artifactId> 			<scope>provided</scope> 		</dependency> 2.启动类继承SpringBootServletInitializer @Override 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) { 		return application.sources(WebApplication.class); 	}

--- web/src/main/java/com/noah/WebApplication.java
+++ web/src/main/java/com/noah/WebApplication.java
@@ -3,14 +3,26 @@ package com.noah;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
 @MapperScan(basePackages = "com.noah.dao.mapper.*")
-public class WebApplication {
+public class WebApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
+	}
+
+	/**
+	 * war包形式部署，继承SpringBootServletInitializer
+	 * @param application
+	 * @return
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebApplication.class);
 	}
 }
